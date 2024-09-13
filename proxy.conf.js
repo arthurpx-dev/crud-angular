53e0e89448185516049c3ab487ac9b168b32814a
Arthur Paixao <arthur.paixao@tre-ma.jus.br>
2024-09-13 11:30:38 -0300

    comentando configurações e ajustando dockerfile e nginx para se conectar com backend através dos conteiners

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,10 +1,17 @@
+// Define a configuração do proxy para o ambiente de desenvolvimento
 const PROXY_CONFIG = [
   {
-    context: ["/api"],
-    target: "http://localhost:8080/",
-    secure: false,
-    logLevel: "debug",
+    // Define o caminho de origem que será interceptado pelo proxy
+    "/api": {
+      // O alvo para onde as solicitações serão redirecionadas
+      target: "http://spring-boot:8080",
+      // Define se o proxy deve usar HTTPS. No seu caso, você está usando HTTP, então isso é `false`
+      secure: false,
+      // Altera o cabeçalho `Origin` para que a origem da solicitação pareça ser a mesma do backend
+      changeOrigin: true,
+    },
   },
 ];
 
+// Exporta a configuração para que o Angular CLI possa usá-la
 module.exports = PROXY_CONFIG;
